d6a4dfeb2131b5d22976d5b781375d344ce9dd4a
Rehan7A <rehanansari6565@gmail.com>
2024-01-17 19:46:01 +0530

    boiler plate fixed

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,34 +1,34 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
-import {useHomeController} from './HomeController';
-import Animated from 'react-native-reanimated';
-import {fonts} from '@constants/fonts';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { useHomeController } from "./HomeController";
+import Animated from "react-native-reanimated";
+import { fonts } from "@constants/fonts";
 
 const Home = () => {
-  const {ranimatedStyle} = useHomeController();
-  return (
-    <View style={styles.container}>
-      <Animated.View style={[styles.box, ranimatedStyle]} />
-      <Text style={styles.text}>{"Hello Dev's"}</Text>
-    </View>
-  );
+	const { ranimatedStyle } = useHomeController();
+	return (
+		<View style={styles.container}>
+			<Animated.View style={[styles.box, ranimatedStyle]} />
+			<Text style={styles.text}>{"Hello Dev's"}</Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    // alignItems:"center",
-  },
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-    borderRadius: 100 / 2,
-  },
-  text: {
-    ...fonts.poppinsBold18,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		// alignItems:"center",
+	},
+	box: {
+		width: 100,
+		height: 100,
+		backgroundColor: "red",
+		borderRadius: 100 / 2,
+	},
+	text: {
+		...fonts.poppinsBold18,
+	},
 });
 
 export default Home;
